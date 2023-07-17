af6e83a85c9cca377da3782e6f1378db473b2fc0
ELMOUDEN1010 <azizelmouden1999@gmail.com>
2023-07-17 17:17:14 +0100

    let's begin

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>{{ message }}</h2>
+        <button @click="changeMessage">Changer le message</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Bonjour, Vue.js!',
+        };
+    },
+    methods: {
+        changeMessage() {
+            this.message = 'Le message a été changé!';
+        },
+    },
+};
+</script>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon application Laravel et Vue.js</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <h1>Bienvenue dans mon application Laravel et Vue.js</h1>
+        <example-component></example-component>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
