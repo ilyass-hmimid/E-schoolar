63c0b4a95ef7f4baeec03ddaae0b1a5754183155
Hmimid <ilyasshmimid95@gmail.com>
2025-08-14 00:30:30 +0200

    Changement G

---
+++ config/vite.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Vite Server Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the Vite development server URL and build path.
+    | These settings determine where your assets will be loaded from in
+    | development and production environments.
+    |
+    */
+
+    'paths' => [
+        'resources/css/app.css',
+        'resources/css/adminlte-custom.css',
+        'resources/js/app.js',
+    ],
+
+    'server' => [
+        'host' => env('VITE_APP_HOST', 'localhost'),
+        'port' => env('VITE_APP_PORT', 5173),
+        'hmr' => env('VITE_DEV_SERVER_URL') ? [
+            'host' => 'localhost',
+            'protocol' => 'ws',
+            'port' => env('VITE_APP_PORT', 5173),
+        ] : null,
+    ],
+
+    'build_path' => 'build',
+];
